3b7dc1e5270810b94071e30dc6d0458cf0df236f
MR-crt <luardes@mail.ru>
2023-09-26 19:40:47 +0300

    minor changes

--- GX/GX_test.py
+++ GX/GX_test.py
@@ -1,4 +1,5 @@
 import great_expectations as gx
+import psycopg2 
 context = gx.get_context()
 
 #postgres_conn = ("postgresql+psycopg2://postgres:password@10.82.0.37:5432/postgres")
@@ -8,9 +9,9 @@ postgres_conn = ("postgresql+psycopg2://postgres:password@10.82.0.37:5432/postgr
 datasource = context.sources.add_postgres(name=datasource_name, connection_string=postgres_conn)
 
 asset_name = "rusina_asset"
-asset_table_name = "fct_current_weather"
+asset_table_name = "rusina_test.fct_current_weather"
 table_asset = datasource.add_table_asset(name=asset_name, table_name=asset_table_name)
 
-asset_query = "SELECT * from fct_current_weather"
+asset_query = "SELECT * from rusina_test.fct_current_weather"
 
 query_asset = datasource.add_query_asset(name=asset_name, query=asset_query)
